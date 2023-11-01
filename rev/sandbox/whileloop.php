df70901eda50aca0730eeeac606ca281139fba3e
BBQ love 69 <74670055+Zaw-creator@users.noreply.github.com>
2023-11-01 23:27:24 +0000

    Create whileloop.php

---
+++ rev/sandbox/whileloop.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // $pw= 0;
+
+    // while($pw<=10){
+    //     if($pw == 5 ){
+    //         echo "five, ";
+    //     }else{
+    //         echo $pw . ", ";
+    //     }
+    //     $pw +=1;
+    // };
+    
+    // $count = 1;
+    // while($count <=10){
+    //     if($count%2 == 0){
+            
+    //         echo $count."this is even"."<br />";
+    //     }else{
+    //         echo $count."this is odd"."<br />";
+    //     }
+    //     // echo $count;
+    //     $count++;
+    // }
+    $a= 4;
+    
+    ?>
+</body>
+</html>
