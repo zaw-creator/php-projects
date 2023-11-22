1e70d54e879cff54559e429ba52cf7fde964bbfd
BBQ love 69 <74670055+zaw-creator@users.noreply.github.com>
2023-11-22 23:59:55 +0000

    test

--- assignnment_test/test.php
+++ assignnment_test/test.php
@@ -11,9 +11,9 @@
     // $access_key = 'c349d13b967b9bab2b8770977579a5c8';
     // $basecurrency='GBP';
 
-    // $apiURL="http://data.fixer.io/api/latest?access_key=c349d13b967b9bab2b8770977579a5c8&%20symbols%20=%20USD";
-    $access_key ="80f8e303ede7a6ba70377c0e3b86c163"; 
-    $apiURL="http://apilayer.net/api/live?access_key=80f8e303ede7a6ba70377c0e3b86c163&source=GBP&currencies=EUR,USD,CAD,PLN&mode=xml";
+    $apiURL="http://data.fixer.io/api/latest?access_key=c349d13b967b9bab2b8770977579a5c8&%20symbols%20=%20USD";
+    // $access_key ="80f8e303ede7a6ba70377c0e3b86c163"; 
+    // $apiURL="http://apilayer.net/api/live?access_key=80f8e303ede7a6ba70377c0e3b86c163&source=GBP&currencies=EUR,USD,CAD,PLN&mode=xml";
     $currencydata= file_get_contents($apiURL);
 
     if($currencydata === false){
@@ -25,7 +25,11 @@
     if( $currencydata === null){
         die("error ");
     }
-    echo json_encode($currencydata);
+    //  can watch at php.net about the flags lol a bit confusing.
+    echo json_encode($currencydata, JSON_FORCE_OBJECT | JSON_PRETTY_PRINT);
+
     ?>
+
+
 </body>
 </html>
---
+++ assignnment_test/test3.php
@@ -0,0 +1,9 @@
+<?php 
+   
+   $car = array("toyota","bmw");
+
+json_encode($car);
+
+   echo $car[0];
+   
+   ?>
